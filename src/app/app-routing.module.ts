c33182ee7004f2fcbe4f70874c0657033c4d1dfb
Telesheva <megoirlik11@gmail.com>
2020-01-14 00:29:37 +0200

    deleted some unused things

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AuthComponent} from './components/auth/auth.component';
-import {PostsListComponent} from './components/posts-list/posts-list.component';
-import {PostFormComponent} from './components/post-form/post-form.component';
-import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
-import {PostComponent} from './components/post/post.component';
 
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./components/auth/auth.module').then(mod => mod.AuthModule) },
   { path: '', loadChildren: () => import('./components/posts-list/posts-list.module').then(mod => mod.PostsListModule) }
-  /*{path: '', component: AuthComponent, pathMatch: 'full'},
-    { path: '**', component: PageNotFoundComponent }*/
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-import { AuthComponent } from './components/auth/auth.component';
-import { ButtonComponent } from './components/UI/button/button.component';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import {environment} from '../environments/environment';
-import { PostsListComponent } from './components/posts-list/posts-list.component';
-import { PostFormComponent } from './components/post-form/post-form.component';
-import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
-import {PostComponent} from './components/post/post.component';
-import { LoaderComponent } from './components/UI/loader/loader.component';
-import {AuthModule} from './components/auth/auth.module';
-import {PostsListModule} from './components/posts-list/posts-list.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  /*  AuthComponent,
-    ButtonComponent,
-    LoaderComponent
-    PostsListComponent,
-    PostFormComponent,
-    PageNotFoundComponent,
-    PostComponent,*/
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
